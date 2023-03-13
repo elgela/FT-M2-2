9a0f3fe9543a5cf4f382400b87ae4be69aafa972
elgela <gelatomarcelo@hotmail.comgit config --global user.name elgelagit config --global user.email gelatomarcelo@hotmail.com>
2023-03-13 15:37:59 -0300

    started

--- 01-DOM/homework/DOMhomework.js
+++ 01-DOM/homework/DOMhomework.js
@@ -1,12 +1,14 @@
 // Crear un array vacío llamado 'toDoItems'
 // Tu codigo acá:
-
+let toDoItems = [];
 
 // En la página 'index.html' hay un elemento span cuyo texto es 'Aplicación creada por:'.
 // Usando querySelector seleccionar dicho span por su id ('createdBy') y luego usando innerHTML
 // agregar tu nombre al final del texto actual. Ej: 'Aplicación creada por Franco'
 // Tu código acá:
-  
+  var nombre = 'Marcelo';
+  var span = document.querySelector('#createdBy');
+  span.innerHTML = span.innerHTML + ' ' + nombre;
 
 
 // Crear una clase denominada 'ToDo' cuyo constructor debe recibir un único parámetro del tipo string
