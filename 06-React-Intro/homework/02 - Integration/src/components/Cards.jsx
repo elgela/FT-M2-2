e017e652674f6ca909db84d5e1c9339d1563faa3
elgela <gelatomarcelo@hotmail.comgit config --global user.name elgelagit config --global user.email gelatomarcelo@hotmail.com>
2023-03-19 22:27:22 -0300

    more cards added

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,9 +1,18 @@
 import Card from './Card';
 
-export default function Cards() {
+export default function Cards(props) {
+   const { characters } = props;
    return (
       <div>
-         
+         {characters.map(character =>
+            <Card key = {character.id} 
+               name = {character.name}
+               status = {character.status}
+               species = {character.species}
+               gender = {character.gender}
+               image = {character.image}
+               onclose = {() => window.alert('Emulamos cerrar la card')}
+            />)}
       </div>
    );
 }
