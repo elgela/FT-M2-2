188b9903894fa86ee16bc02fbb1440a6e3e671f3
elgela <gelatomarcelo@hotmail.comgit config --global user.name elgelagit config --global user.email gelatomarcelo@hotmail.com>
2023-03-21 11:04:09 -0300

    solved

--- 06-React-Intro/homework/02 - Integration/src/App.js
+++ 06-React-Intro/homework/02 - Integration/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import characters from './data.js';
+// import Nav from './components/Nav.jsx';
 
 function App() {
    return (
