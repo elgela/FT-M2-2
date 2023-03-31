acf0d12327683eb2b5424c014621685ee433b2ed
elgela <gelatomarcelo@hotmail.comgit config --global user.name elgelagit config --global user.email gelatomarcelo@hotmail.com>
2023-03-30 21:45:19 -0300

    empezado, con errores

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './Contact.modules.css'
 
-// eslint-disable-next-line
-const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export default function Contact () {
-  return <div>Crear Formulario</div>
+// eslint-disable-next-line
+// const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+const [inputs, setImputs] = React.useState({
+  name: '',
+  email: '',
+  message: '',
+})
+
+const [errors, setErrors] = React.useState({
+  name: '',
+  email: '',
+  message: '',
+})
+
+export default function Contact() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Nombre</label>
+        <input type="text" name='name' placeholder='Marcelo' />
+        <label htmlFor="">Correo electrónico</label>
+        <input className='name' value={inputs.name} type="text" name='email' placeholder='Escribe tu email'  />
+        <label htmlFor="">Mensaje:</label>
+        <textarea type="text" name="message" placeholder='Escribe tu mensaje' ></textarea>
+        <button type='submit'>enviar</button>
+      </form>
+    </div>)
 }
