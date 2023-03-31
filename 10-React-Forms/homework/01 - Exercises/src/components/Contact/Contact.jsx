df743481e9e4250e5944c7b0dcc149a0f63e5b62
elgela <gelatomarcelo@hotmail.com>
2023-03-31 10:43:24 -0300

    Contact modified

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,33 +1,24 @@
 import React from 'react'
 import './Contact.modules.css'
 
-
 // eslint-disable-next-line
-// const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-const [inputs, setImputs] = React.useState({
-  name: '',
-  email: '',
-  message: '',
-})
-
-const [errors, setErrors] = React.useState({
-  name: '',
-  email: '',
-  message: '',
-})
 
 export default function Contact() {
   return (
     <div>
-      <form action="">
-        <label htmlFor="">Nombre</label>
-        <input type="text" name='name' placeholder='Marcelo' />
-        <label htmlFor="">Correo electrónico</label>
-        <input className='name' value={inputs.name} type="text" name='email' placeholder='Escribe tu email'  />
-        <label htmlFor="">Mensaje:</label>
-        <textarea type="text" name="message" placeholder='Escribe tu mensaje' ></textarea>
-        <button type='submit'>enviar</button>
+      <form>
+        <label>Nombre</label>
+        <input  className='name' placeholder='Escribe tu nombre...' type="text"/>
+        <label>Correo Electrónico</label>
+        <input className='email' placeholder='Escribe tu email' type="text" />
+        <label>Mensaje:</label>
+        <textarea  className="message" placeholder='Escribe tu mensaje...' type="text"></textarea>
+        <button type='submit'>Enviar</button>
       </form>
     </div>)
+
+
 }
+
